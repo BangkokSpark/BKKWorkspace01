2c9676834172425c3fd0772f2cc87a1297f97f89
BangkokSpark <44794987+BangkokSpark@users.noreply.github.com>
2018-11-27 02:16:11 +0700

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,4 +1,19 @@
 <?php
-    Print "Hello, World!";
+    // SERVER
+
+if(getenv('REQUEST_METHOD') == 'POST') {
+	$client_data = file_get_contents("php://input");
+	echo "
+		<SERVER>
+			Hallo, I am server
+			This is what I've got from you
+			$client_data
+		</SERVER>
+	";
+	exit();
+}
+
+
+Print "Hello, World!";
     echo "Welcome, I am connecting Android to PHP, MySQL";
 ?>
